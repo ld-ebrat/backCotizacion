0cedda91351a8573265750efafe807746ad6e191
ld-ebrat <ebrat.ld@gmail.com>
2024-02-06 14:49:59 -0500

    update cors

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ const routes = require("./routes/index.js")
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
-  });
+});
 app.use(Express.json())
 app.use('/images', Express.static(path.join(__dirname, 'images')));
 routes(app)
 
 
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
     res.send("Hola")
 })
 
-app.listen(4000, ()=>{
+app.listen(4000, () => {
     console.log("Corriendo en el puesto 4000")
 })
