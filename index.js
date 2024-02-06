e8b8b14f037062d3833a9b11494cc27502ec8bf8
ld-ebrat <ebrat.ld@gmail.com>
2024-02-06 14:28:27 -0500

    update cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config()
 const routes = require("./routes/index.js")
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://front-cotizacion.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
