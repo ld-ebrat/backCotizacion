fea2dca6390d267b9f2c86fbf550a57e700e6995
ld-ebrat <ebrat.ld@gmail.com>
2024-02-06 14:10:27 -0500

    add cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const routes = require("./routes/index.js")
 const corsOptions = {
     origin: '*',
     methods: ['POST', 'GET', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization','Origin','Access-Control-Allow-Origin','Accept','Options','X-Requested-With']
 }
 app.use(cors(corsOptions))
 app.use(Express.json())
